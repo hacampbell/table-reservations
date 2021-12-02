784559adfa49b87be9c6e6de0c1c036dbb8d30a3
Henry Campbell <hacampbell99@gmail.com>
2021-12-03 09:55:58 +1100

    Setup to use JSON

--- server/index.js
+++ server/index.js
@@ -5,12 +5,17 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+// Set up to be compatible with JSON
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+
+// Database URL
 const mongoDbUrl = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}`;
 
-
-
+// 'Quick & Dirty' test route to ensure that the API is up. Remove before putting in production
 app.get('/', (req, res) => {
   res.json({message: 'Hello World!'});
 });
 
+// Listen for connections to our web server.
 app.listen(port, () => console.log(`Express is listening on port ${port}!`));
