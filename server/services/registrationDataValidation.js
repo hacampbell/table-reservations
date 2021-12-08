394722b8fbd7aaf9f38c78c8e46fed57b0cd3346
Henry Campbell <hacampbell99@gmail.com>
2021-12-08 14:57:21 +1100

    Added stubs for data validation functions for user registration

---
+++ server/services/registrationDataValidation.js
@@ -0,0 +1,37 @@
+/**
+ * Checks if a given username is valid i.e. is between 3 and 25 characters inclusive.
+ * @param {string} username The username to check for validity
+ * @returns true if username is valid, otherwise false
+ */
+exports.isValidUsername = (username) => {
+    return 501;
+}
+
+/**
+ * Checks if a username is unique, i.e. that it does not currently exist in the database
+ * @param {string} username The username to check for uniqueness
+ * @returns true if the username does not exist in the database, otherwise false
+ */
+exports.isUniqueUsername = (username) => {
+    return 501;
+}
+
+/**
+ * Checks that a given email is a valid email, i.e. follows the standard email address
+ * convention of 'name@domain.extension'
+ * @param {string} email The email to check for validity 
+ * @returns true if the email is valid, otherwise false
+ */
+exports.isValidEmail = (email) => {
+    return 501;
+}
+
+/**
+ * Checks that a users password is valid, i.e. is between 8 and 25 characters and
+ * contains both letters and numbers
+ * @param {string} password 
+ * @returns true if the password entered is valid, otherwise false
+ */
+exports.isValidPassword = (password) => {
+    return 501;
+}
