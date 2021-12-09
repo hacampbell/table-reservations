6dbb31f6a434d381d6df2fbea017527f6a8abd15
Henry Campbell <hacampbell99@gmail.com>
2021-12-09 15:43:50 +1100

    Completed isValidEmail

--- server/services/registrationDataValidation.js
+++ server/services/registrationDataValidation.js
@@ -30,7 +30,10 @@ exports.isUniqueUsername = async (username) => {
  * @returns true if the email is valid, otherwise false
  */
 exports.isValidEmail = (email) => {
-    return 501;
+    const trimmedEmail = email.trim();
+    // RFC 5322 Complaiant, sourced from emailregex.com
+    const expression = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+    return expression.test(trimmedEmail);
 }
 
 /**
