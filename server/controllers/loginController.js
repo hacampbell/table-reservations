2c77138eb8bcf8b3a9c17101d85b117c78e5066b
Henry Campbell <hacampbell99@gmail.com>
2021-12-13 14:01:00 +1100

    Added basic logic to send JWT to user on login for future request authorisation

--- server/controllers/loginController.js
+++ server/controllers/loginController.js
@@ -1,6 +1,9 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 const User = require('../models/User'); 
+
+const {ACCESS_TOKEN_SECRET, REFRESH_TOKEN_SECRET} = require('../config/config');
 
 /**
  * Logs a user in
@@ -12,11 +15,23 @@ exports.Login = async (req, res) => {
         const user = res.user;
 
         if (await bcrypt.compare(req.body.password, user.password)) {
-            res.status(200).json({message: 'Logged in'});
+            const token = GenerateJWT(user.username);
+            res.status(200).json({message: 'Logged in', accessToken: token});
         } else {
             res.status(403).json({message: 'Incorrect password'});
         }
     } catch (err) {
         res.status(500).json({message: 'Unable to login.', error: err.message});
     }
+}
+
+/**
+ * 
+ * @param {string} username The username of the user to generate the access token for
+ * @returns A signed access token to be sent to the user to authorise future requests
+ */
+function GenerateJWT (username) {
+    const userObj = {'username': username};                         // Serialise user information needed for the token
+    const accessToken = jwt.sign(userObj, ACCESS_TOKEN_SECRET);     // Create JWT
+    return accessToken;                                             // Return the token
 }
