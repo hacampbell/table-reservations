027ba2039ee0006c190843ceebabaf26930ebdfa
Henry Campbell <hacampbell99@gmail.com>
2021-12-15 13:03:55 +1100

    Added FindByRefreshToken

--- server/services/findUserMiddleware.js
+++ server/services/findUserMiddleware.js
@@ -2,7 +2,7 @@ const User = require('../models/User');
 
 /**
  * A small middleware function for finding a user in the database by their username.
- * Adds the data for said user to the res object for user in the next function.
+ * Adds the data for said user to the res object for use in the next function.
  * @param {*} req The users request
  * @param {*} res The result
  * @param {*} next The next function to call 
@@ -12,11 +12,32 @@ exports.GetUserByUsername = async (req, res, next) => {
     try {
         const user = await User.findOne({username: req.body.username});
         if (user == null) {
-            return res.status(404).json({message: "User not found"});
+            return res.status(404).json({message: 'User not found'});
         }
         res.user = user;
         next();
     } catch (err) {
-        res.status(500).json({message: "An error occured trying to find user", error: err.message});
+        res.status(500).json({message: 'An error occured trying to find user', error: err.message});
+    }
+}
+
+/**
+ * A small middleware function for finding a user in the database by their refresh
+ * token. Adds the data for said user to the res object for use in the next function.
+ * @param {*} req The users request
+ * @param {*} res The result
+ * @param {*} next The next function to call 
+ * @returns Status 403 with message invalid refresh token if no matching user is found for the given refresh token.
+ */
+exports.GetUserByRefreshToken = async (req, res, next) => {
+    try {
+        const user = await User.findOne({refreshToken: req.body.refreshToken});
+        if (user == null) {
+            return res.status(403).json({message: 'Invalid refresh token'});
+        }
+        res.user = user;
+        next();
+    } catch (err) {
+        res.status(500).json({message: 'An error occured trying to find user', error: err.message});
     }
 }
