dbfc8b86248799ce179fe8ce65e443452c692e3e
Henry Campbell <hacampbell99@gmail.com>
2022-01-11 10:41:53 +1100

    Added stubs for required functionality

---
+++ server/controllers/restaurantController.js
@@ -0,0 +1,37 @@
+const Restaurant = require('../models/Restaurant');
+
+/**
+ * Gets a list of all restaurants in the database
+ * @param {*} req 
+ * @param {*} res 
+ */
+exports.GetAllRestaurants = async (req, res) => {
+    res.status(501).json({message: 'Implementation pending'});
+}
+
+/**
+ * Creates a new restaurant in the database
+ * @param {*} req 
+ * @param {*} res 
+ */
+exports.CreateRestaurant = async (req, res) => {
+    res.status(501).json({message: 'Implementation pending'});
+}
+
+/**
+ * Deletes a given restaurant in the database
+ * @param {*} req 
+ * @param {*} res 
+ */
+exports.DeleteRestaurant = async (req, res) => {
+    res.status(501).json({message: 'Implementation pending'});
+}
+
+/**
+ * Updates a given restaurant in the database
+ * @param {*} req 
+ * @param {*} res 
+ */
+exports.UpdateRestaurant = async (req, res) => {
+    res.status(501).json({message: 'Implementation pending'});
+}
