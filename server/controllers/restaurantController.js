148a2080f32acd2b37d02780989aed4203dfa549
Henry Campbell <hacampbell99@gmail.com>
2022-01-14 11:50:19 +1100

    Added logic for updating restaurant information

--- server/controllers/restaurantController.js
+++ server/controllers/restaurantController.js
@@ -70,5 +70,26 @@ exports.DeleteRestaurant = async (req, res) => {
  * @param {*} res 
  */
 exports.UpdateRestaurant = async (req, res) => {
-    res.status(501).json({message: 'Implementation pending'});
+    const noEditFields = ['__v', '_id']; // List of fields we won't allow the user to edit
+
+    // Make sure only admins and the owner of a restaurant can update it's information
+    if (res.restaurant.owner !== res.user.username && res.user.role !== 'admin') {
+        return res.status(403).json({message: 'You do not have permission to perform that operation'});
+    }
+
+    try {
+        // Loop through the new data and update each field we've been sent that differs
+        // from what's currently stored in the database.
+        for (item in req.body) {
+            if (req.body[item] !== res.restaurant[item] && res.restaurant[item] != undefined && !noEditFields.includes(item)) {
+                res.restaurant[item] = req.body[item];
+            }
+        }
+
+        // Save the newly updated restaurant information and inform the user
+        await res.restaurant.save();
+        res.status(200).json({message: 'Restaurant updated'});
+    } catch (err) {
+        res.status(500).json({message: 'An error occured trying to update the restaurant', error: err.message});
+    }
 }
