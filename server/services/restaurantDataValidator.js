1016f9530f82c699c62a2c4d04d35c43ef53b4c8
Henry Campbell <hacampbell99@gmail.com>
2022-01-16 09:57:00 +1100

    Added stubs for data validation functions

---
+++ server/services/restaurantDataValidator.js
@@ -0,0 +1,69 @@
+/**
+ * Checks that the name of a restaurant is valid i.e. is at least 2 charcters
+ * @param {String} name The name of the restaurant
+ */
+exports.isValidRestaurantName = (name) => {
+
+}
+
+/**
+ * Checks that the opening hours of the restaurant are valid i.e. is of the
+ * format 'xx:xx-xx:xx'
+ * @param {String} hours The hours a restaurant is open
+ */
+exports.isValidOpeningHours = (hours) => {
+
+}
+
+/**
+ * Checks that the opening days of the restaurant are valid. More thought is
+ * required for how this will actually function in the working application,
+ * so currently in the interest of getting a working demo this will always
+ * return true.
+ * @param {String} days The days a restaurant is open
+ */
+exports.isValidOpeningDays = (days) => {
+    return true;
+}
+
+/**
+ * Checks that the contact number entered for a restaurant is valid i.e. is 
+ * only numbers and is 10 digits long.
+ * @param {String} contact The contact phone number of the restaurant
+ */
+exports.isValidContactNumber = (contact) => {
+
+}
+
+/**
+ * Checks that a valid address has been entered for a restaurant i.e. is a
+ * string of the format '[number] [street] [street type], [town]' 
+ * e.g. '42 Demo Street, Demo City'
+ * @param {String} address The address of the restaurant
+ */
+exports.isValidAddress = (address) => {
+
+}
+
+/**
+ * Checks that a valid description of the restaurant has been entered i.e. is
+ * not blank or more than 512 characters
+ * @param {String} description The description of the restaurant
+ */
+exports.isValidDescription = (description) => {
+
+}
+
+/**
+ * Checks that a valid path for an image has been specified i.e. is of the
+ * format 'src/assets/img/name.jpg' or 'src/assets/img/name.png'
+ * 
+ * This is not an overly user friendly approach for validating this field, 
+ * however in the interest of getting a working demo, it will be used. The way 
+ * that images for restaurants are handled in an actual production setting 
+ * would need to be very different.
+ * @param {String} image The path for an image of the restaurant 
+ */
+exports.isValidImage = (image) => {
+    
+}
