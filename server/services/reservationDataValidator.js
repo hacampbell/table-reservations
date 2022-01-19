5265613ae60c0949f0ce58620dee9fae287a331d
Henry Campbell <hacampbell99@gmail.com>
2022-01-19 13:42:14 +1100

    Reverted implemenation of isValidRestaurant

--- server/services/reservationDataValidator.js
+++ server/services/reservationDataValidator.js
@@ -1,4 +1,5 @@
-const moment = require('moment');
+const moment = require("moment");
+const findRestaurant = require('../services/findRestaurantMiddleware');
 
 /**
  * Checks that a valid name for a reservation has been given. Valid names are
@@ -62,7 +63,8 @@ exports.isValidGuests = (guests) => {
  * @returns true if the name of a restaurant is valid
  */
 exports.isValidRestaurant = async (restaurantName) => {
-    return res.restaurant !== [] && res.restaurantName === restaurantName;
+    result = await findRestaurant.GetRestaurantByName(restaurantName);
+    return result != null;
 }
 
 /**
