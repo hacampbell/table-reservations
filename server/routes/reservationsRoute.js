299c129c7dd8b933e0d6bab622c840951325b82b
Henry Campbell <hacampbell99@gmail.com>
2022-01-22 14:32:27 +1100

    Added findRestaurant middleware for post and patch routes

--- server/routes/reservationsRoute.js
+++ server/routes/reservationsRoute.js
@@ -7,7 +7,8 @@ const reservationsController = require('../controllers/reservationsController');
 // Services / Middleware
 const authHandler = require('../services/authorisationHandler');
 const userFinder = require('../services/findUserMiddleware');
-const resFinder = require('../services/findReservationMiddleware');
+const resvFinder = require('../services/findReservationMiddleware');
+const restFinder = require('../services/findRestaurantMiddleware');
 const dataValidator = require('../services/reservationDataValidator');
 
 // Routes
@@ -16,15 +17,15 @@ const dataValidator = require('../services/reservationDataValidator');
 router.get('/', authHandler.ValidateAuthToken, userFinder.GetUserByUsername, reservationsController.GetAllReservations);
 
 // Get single reservation by id 
-router.get('/:id', authHandler.ValidateAuthToken, userFinder.GetUserByUsername, resFinder.GetReservationByID, reservationsController.GetSingleReservation);
+router.get('/:id', authHandler.ValidateAuthToken, userFinder.GetUserByUsername, resvFinder.GetReservationByID, reservationsController.GetSingleReservation);
 
 // Create a reservation
-router.post('/', authHandler.ValidateAuthToken, userFinder.GetUserByUsername, dataValidator.ValidatePostData, reservationsController.CreateReservation);
+router.post('/', authHandler.ValidateAuthToken, userFinder.GetUserByUsername, restFinder.GetRestaurantByName, dataValidator.ValidatePostData, reservationsController.CreateReservation);
 
 // Update a reservation by id
-router.patch('/:id', authHandler.ValidateAuthToken, userFinder.GetUserByUsername, resFinder.GetReservationByID, dataValidator.ValidatePatchData, reservationsController.UpdateReservation);
+router.patch('/:id', authHandler.ValidateAuthToken, userFinder.GetUserByUsername, restFinder.GetRestaurantByName, resvFinder.GetReservationByID, dataValidator.ValidatePatchData, reservationsController.UpdateReservation);
 
 // Cancel a reservation by id
-router.delete('/:id', authHandler.ValidateAuthToken, userFinder.GetUserByUsername, resFinder.GetReservationByID, reservationsController.CancelReservation);
+router.delete('/:id', authHandler.ValidateAuthToken, userFinder.GetUserByUsername, resvFinder.GetReservationByID, reservationsController.CancelReservation);
 
 module.exports = router;
