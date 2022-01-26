3c8a95a84171978d51511e76238712539c2d39c3
Henry Campbell <hacampbell99@gmail.com>
2022-01-26 12:52:31 +1100

    Added form and basic CSS

--- client/src/views/LoginScreen.vue
+++ client/src/views/LoginScreen.vue
@@ -1,7 +1,21 @@
 <template>
     <div class="background">
         <div class="login-container">
-            <h1>Test</h1>
+            <h1>Login</h1>
+
+            <form>
+                <div class="form-group">
+                    <label for="username">Username</label>
+                    <input type="text" class="form-control" id="username" aria-describedby="username" placeholder="Enter username">
+                </div>
+
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" class="form-control" id="password" placeholder="Password">
+                </div>
+
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
         </div>
     </div>
 </template>
@@ -22,6 +36,22 @@
         margin: 0px 0px 0px 0px;
         padding: 0px 0px 0px 0px;
         border: 0px 0px 0px 0px;
+    }
+
+    form {
+        margin-top: 15%;
+        margin-left: 8%;
+        max-width: 85%;
+    }
+
+    label {
+        margin-top: 5%;
+        margin-bottom: 1%;
+    }
+
+    button {
+        margin-top: 5%;
+        min-width: 20%;
     }
 
     .background {
