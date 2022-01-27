9fdd16e6aebee8a29cae9349e6083d7f941b5f10
Henry Campbell <hacampbell99@gmail.com>
2022-01-27 11:39:57 +1100

    Broke Login form out into component

---
+++ client/src/components/auth/Login.vue
@@ -0,0 +1,90 @@
+<template>
+    <div class="login-container">
+        <h1>Login</h1>
+
+        <form @submit.prevent="Login">
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" class="form-control" id="username" placeholder="Enter username" v-model="username">
+            </div>
+
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" id="password" placeholder="Password" v-model="password">
+            </div>
+
+            <button type="submit" class="btn btn-primary">Login</button>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Login',
+        
+        data() {
+            // Define fields for our data binding model
+            return {
+                username: '',
+                password: ''
+            }
+        },
+
+        methods: {
+            // Used to send the details entered by the user to the server for login
+            async Login () {
+                const loginRoute = 'http://localhost:3000/user/login';
+
+                // Craft the payload we want to send
+                const payload = {
+                    username: this.username,
+                    password: this.password
+                }
+
+                // Make the request
+                const response = await fetch(loginRoute, {
+                    method: 'POST',
+                    headers: {'Content-Type': 'application/json'},
+                    body: JSON.stringify(payload)
+                });
+
+                // Process and just display the result for now
+                const respData = await response.json();
+                console.log(respData);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    form {
+        margin-top: 15%;
+        margin-left: 8%;
+        max-width: 85%;
+    }
+
+    label {
+        margin-top: 5%;
+        margin-bottom: 1%;
+    }
+
+    button {
+        margin-top: 5%;
+        min-width: 20%;
+    }
+
+    .login-container {
+        background-color: rgb(214, 214, 214);
+        width: 35%;
+        height: 50%;
+        justify-content: center;
+        border-radius: 5%;
+    }
+
+    @media only screen and (max-width: 900px) {
+        .login-container {
+            width: 70%;
+            height: 70%
+        }
+    }
+</style>
--- client/src/views/LoginScreen.vue
+++ client/src/views/LoginScreen.vue
@@ -1,84 +1,25 @@
 <template>
     <div class="background">
-        <div class="login-container">
-            <h1>Login</h1>
-
-            <form @submit.prevent="Login">
-                <div class="form-group">
-                    <label for="username">Username</label>
-                    <input type="text" class="form-control" id="username" placeholder="Enter username" v-model="username">
-                </div>
-
-                <div class="form-group">
-                    <label for="password">Password</label>
-                    <input type="password" class="form-control" id="password" placeholder="Password" v-model="password">
-                </div>
-
-                <button type="submit" class="btn btn-primary">Login</button>
-            </form>
-        </div>
+        <Login />
     </div>
 </template>
 
-
 <script>
+    import Login from '../components/auth/Login.vue';
+
     export default {
-        data() {
-            // Define fields for our data binding model
-            return {
-                username: '',
-                password: ''
-            }
-        },
-
-        methods: {
-            // Used to send the details entered by the user to the server for login
-            async Login () {
-                const loginRoute = 'http://localhost:3000/user/login';
-
-                // Craft the payload we want to send
-                const payload = {
-                    username: this.username,
-                    password: this.password
-                }
-
-                // Make the request
-                const response = await fetch(loginRoute, {
-                    method: 'POST',
-                    headers: {'Content-Type': 'application/json'},
-                    body: JSON.stringify(payload)
-                });
-
-                // Process and just display the result for now
-                const respData = await response.json();
-                console.log(respData);
-            }
+        name: 'LoginScreen',
+        components: {
+            Login
         }
     }
 </script>
-
 
 <style>
     body {
         margin: 0px 0px 0px 0px;
         padding: 0px 0px 0px 0px;
         border: 0px 0px 0px 0px;
-    }
-
-    form {
-        margin-top: 15%;
-        margin-left: 8%;
-        max-width: 85%;
-    }
-
-    label {
-        margin-top: 5%;
-        margin-bottom: 1%;
-    }
-
-    button {
-        margin-top: 5%;
-        min-width: 20%;
     }
 
     .background {
@@ -89,20 +30,5 @@
         display: flex;
         align-items: center;
         justify-content: center;
-    }
-
-    .login-container {
-        background-color: rgb(214, 214, 214);
-        width: 35%;
-        height: 50%;
-        justify-content: center;
-        border-radius: 5%;
-    }
-
-    @media only screen and (max-width: 900px) {
-        .login-container {
-            width: 70%;
-            height: 70%
-        }
     }
 </style>
