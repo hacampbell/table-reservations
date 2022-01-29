bce2401ee6e959e56f2844fc9c6dd801ad78d511
Henry Campbell <hacampbell99@gmail.com>
2022-01-29 11:15:34 +1100

    Added stores for access and refresh tokens

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,12 +1,48 @@
 import { createStore } from 'vuex'
 
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {
+        accessToken: '',   // Token to be sent with Authorization header
+        refreshToken: '',  // Token used to efresh access token after it expires
+    },
+
+    mutations: {
+        //Stores a given access token in the vuex store
+        SetAccessToken (state, newToken) {
+            state.accessToken = newToken;
+        },
+
+        //Stores a given refresh token in the vuex store
+        SetRefreshToken (state, newToken) {
+            state.refreshToken = newToken;
+        }
+    },
+
+    actions: {
+        // Sets the access token
+        SetAccessToken ({commit}, newToken) {
+            commit('SetAccessToken', newToken);
+        },
+
+        // Sets the refresh token
+        SetRefreshToken ({commit}, newToken) {
+            commit('SetRefreshToken', newToken);
+        }
+    },
+
+    getters: {
+
+        //Gets the stored access token
+        GetAccessToken (state) {
+            return state.accessToken;
+        },
+
+        //Gets the stored refresh token
+        GetRefreshToken (state) {
+            return state.refreshToken
+        }
+    },
+
+    modules: {
+    }
 })
