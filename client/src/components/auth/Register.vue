24702dfa44e454d71aca0dfaaea6a78ef18003ea
Henry Campbell <hacampbell99@gmail.com>
2022-01-29 11:36:47 +1100

    Added register component

---
+++ client/src/components/auth/Register.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="register-container">
+        <h1>Drop Bear Table</h1>
+        <img src="@/assets/img/koala.png" />
+
+        <h2>Sign Up</h2>
+
+        <form @submit.prevent="SendRegisterDetails">
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="text" class="form-control" id="email" placeholder="Email..." v-model="email">
+            </div>
+
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" class="form-control" id="username" placeholder="Username..." v-model="username">
+            </div>
+
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" id="password" placeholder="Password..." v-model="password">
+            </div>
+
+            <button type="submit" class="btn btn-primary">Login</button>
+        </form>
+    </div>
+</template>
+
+<script>
+    import {Register} from '../../services/auth';
+
+    export default {
+        name: 'Register',
+
+        data() {
+            // Define fields for our data binding model
+            return {
+                email: '',
+                username: '',
+                password: ''
+            }
+        },
+
+        methods: {
+            // Used to send the details to create a new user
+            async SendRegisterDetails () {
+                const response = await Register(this.email, this.username, this.password);
+                console.log(response);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    img {
+        max-width: 50%;
+    }
+
+    h2 {
+        margin-top: 5%;
+        margin-bottom: 0;
+    }
+
+    label {
+        margin: 2% 5% 0.5% 5%;
+    }
+
+    button {
+        margin: 5%;
+        min-width: 25%;
+    }
+
+    .register-container {
+        background-color: rgb(216, 215, 215);
+        min-width: 35%;
+        min-height: 60%;
+        border-radius: 3%;
+        padding: 5%;
+    }
+</style>
