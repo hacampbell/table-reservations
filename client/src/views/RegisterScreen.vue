66df1350b2f4d10c986cdf4b0a1b2484ddf6f670
Henry Campbell <hacampbell99@gmail.com>
2022-01-29 11:37:04 +1100

    Added register view

---
+++ client/src/views/RegisterScreen.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="background">
+        <Register />
+    </div>
+</template>
+
+<script>
+    import Register from '../components/auth/Register.vue';
+
+    export default {
+        name: 'RegisterScreen',
+        components: {
+            Register
+        }
+    }
+</script>
+
+<style>
+    .background {
+        background-color: rgb(35, 39, 61);
+        position: fixed;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        overflow-y: auto;
+    }
+</style>
