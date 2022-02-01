e50548d4c66e08d0c436a6ba7f0d477cf433f8db
Henry Campbell <hacampbell99@gmail.com>
2022-02-01 11:58:25 +1100

    Allowed Authorization header in CORS policy

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ app.use( (req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'POST, GET, DELETE, OPTIONS, PATCH')
 
     //Access-Control-Allow-Headers
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
 
     next()
 });
