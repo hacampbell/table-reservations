84a20f60106b006c25ec6303348d70ce7380a80d
Henry Campbell <hacampbell99@gmail.com>
2022-02-01 12:58:40 +1100

    Setup to display restaurants sent from the server

--- client/src/views/RestaurantsScreen.vue
+++ client/src/views/RestaurantsScreen.vue
@@ -1,26 +1,62 @@
 <template>
     <div class="container-fluid">
-        <div class="row">
+        <div id="content" class="row">
             <SideNav activePage="restaurants" />
             <div class="col">
-                <h2>Test</h2>
+                <div id="card-area" class="row row-cols-1 row-cols-md-3 g-4">
+                    <RestaurantCard v-for="rest in this.restaurantList" :key="rest._id"
+                    :restName="rest.name"
+                    :description="rest.description" 
+                    image="dorsia.jpeg" 
+                    />
+                </div>
             </div>
         </div>
     </div>        
 </template>
 
+<style scoped>
+    #card-area {
+        padding-top: 1%;
+    }
+</style>
+
 <script>
     import SideNav from '../components/nav/SideNav.vue';
+    import RestaurantCard from '../components/restaurants/RestaurantCard.vue';
     import {GetRestaurants} from '../services/restaurants';
 
     export default {
         name: 'Restaurants',
         components: {
-            SideNav
+            SideNav,
+            RestaurantCard
         },
+
         async created() {
-            const restraunts = await GetRestaurants(this.$store.getters.GetAccessToken);
-            console.log(restraunts);
+            // Get a list of restaurants from the server
+            const restaurants = await GetRestaurants(this.$store.getters.GetAccessToken);
+            console.log(restaurants);
+            
+            // If we couldn't get the restaurant list, just display an error for now
+            if (!restaurants) {
+                this.$toast.open({
+                        message: `Unable to get restaurants.`,
+                        type: 'error',
+                        duration: 5000
+                });
+                return;
+            }
+
+            // If all went well, set the data for display
+            this.restaurantList = restaurants;
+        },
+
+        data () {
+            // Store a list of restaurants that we can loop over and display
+            return {
+                restaurantList: []
+            }
         }
     }
 </script>
