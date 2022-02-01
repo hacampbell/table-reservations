efc5e54f54a638633dc9d8bf98ffc2d04aa1c16d
Henry Campbell <hacampbell99@gmail.com>
2022-02-01 11:29:17 +1100

    Added restaurants view

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -4,33 +4,26 @@ const routes = [
     {
         path: '/',
         name: 'Home',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "home" */ '../views/Home.vue')
     },
     {
         path: '/login',
         name: 'Login',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "login" */ '../views/LoginScreen.vue')
     },
     {
         path: '/register',
         name: 'Register',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "register" */ '../views/RegisterScreen.vue')
+    },
+    {
+        path: '/restaurants',
+        name: 'Restaurants',
+        component: () => import(/* webpackChunkName: "restaurants" */ '../views/RestaurantsScreen.vue')
     },
     {
         path: '/:pathMatch(.*)*',
         name: 'PageNotFound',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "pagenotfound" */ '../views/PageNotFound.vue')
     }
 ]
---
+++ client/src/views/RestaurantsScreen.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <SideNav activePage="restaurants" />
+            <div class="col">
+                <h2>Test</h2>
+            </div>
+        </div>
+    </div>        
+</template>
+
+<script>
+    import SideNav from '../components/nav/SideNav.vue';
+
+    export default {
+        name: 'Restaurants',
+        components: {
+            SideNav
+        }
+    }
+</script>
